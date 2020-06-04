9d89104460d54104a2183c4e72df62f26c7c47a7
José Américo Lacerda Félix <joseamericolf@gmail.com>
2020-06-04 17:15:14 -0300

    port from enviroment variable

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const fetch = require('node-fetch');
 require('dotenv').config();
 
 const app = express();
-app.listen(3000, () => console.log('listening at 3000'));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Ouvindo na porta ${port}`));
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
 
